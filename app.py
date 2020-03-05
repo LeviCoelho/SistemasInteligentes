229f4925a53c98c217b3181d50804985771ccec9
LeviCoelho <coelhobelem@hotmail.com>
2020-03-05 18:47:47 -0300

    Master atualizada após erro

--- app.py
+++ app.py
@@ -13,17 +13,23 @@ D_Rocha = dataset_Rocha.iloc[:, 60:].values
 #Preparando dados Iris
 dataset_Iris = pd.read_csv('databases/iris.data') # caminho relativo do arquivo q está na mesma pasta do programa
 dataset_Iris.replace(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'], [1, -1, -1], inplace=True)
+X = dataset_Iris.values
+d = dataset_Iris.values
+x = np.arange(len(dataset_Iris))
+np.random.shuffle(x)
 
-X_Iris = dataset_Iris.iloc[0:101, 0:4].values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
-np.random.shuffle(X_Iris)
+X_new = X[x]
+d_new = d[x]
 
-D_Iris = dataset_Iris.iloc[0:101, 4:].values
-np.random.shuffle(D_Iris)
+X_Iris = X_new.iloc[0:101, 0:4].values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
+#np.random.shuffle(X_Iris)
+D_Iris = d_new.iloc[0:101, 4:].values
+#np.random.shuffle(D_Iris)
 
-X_IrisTest = dataset_Iris.iloc[101:151, 0:4].values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
-np.random.shuffle(X_IrisTest)
-D_IrisTest = dataset_Iris.iloc[101:151:, 4:].values
-np.random.shuffle(D_IrisTest)
+X_IrisTest = X_new.iloc[101:151, 0:4].values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
+#np.random.shuffle(X_IrisTest)
+D_IrisTest = d_new.iloc[101:151:, 4:].values
+#np.random.shuffle(D_IrisTest)
 #Adaline
     #Adaline Iris
 print('Rodando Adaline para dados - Iris')
@@ -45,14 +51,14 @@ a_Rocha.Graphic()
 a_Rocha.showGraphic()
 print('')
 
-#Perceptron
+Perceptron
     #Perceptron Iris
-#print('Rodando Perceptron para dados - Iris')
-#p_Iris = Perceptron(len(X_Iris[0]), epochs=100, labelGraphic = 'Perceptron - Iris') #epochs, quantidade de iteraçoes
-#p_Iris.train(X_Iris, D_Iris)
-#p_Iris.testNetwork(X_IrisTest,D_IrisTest)
-#p_Iris.showConfusionMatrix()
-#print('')
+print('Rodando Perceptron para dados - Iris')
+p_Iris = Perceptron(len(X_Iris[0]), epochs=100, labelGraphic = 'Perceptron - Iris') #epochs, quantidade de iteraçoes
+p_Iris.train(X_Iris, D_Iris)
+p_Iris.testNetwork(X_IrisTest,D_IrisTest)
+p_Iris.showConfusionMatrix()
+print('')
     #Perceptron Rochas
 #print('Rodando Perceptron para dados - Rochas')
 
@@ -60,5 +66,4 @@ print('')
 #p_Rocha.train(X_Rocha, D_Rocha)
 #print('')
 print('Finalizado')
-
 
