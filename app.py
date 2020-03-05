df5ab9bec5195b191d6259f4c8c0b384b6ece933
LeviCoelho <coelhobelem@hotmail.com>
2020-03-05 19:33:15 -0300

    Adaline e perceptron funcionando com correção no erro que bagunçavas os dados. perceptron so está rodando o banco de dados rochas

--- app.py
+++ app.py
@@ -4,32 +4,43 @@ from perceptron import Perceptron# importe o arquivo perceptron classe Perceptro
 from adaline import Adaline
 
 #Preparando dados Rochas
-dataset_Rocha = pd.read_csv('databases/sonar.all-data') # caminho relativo do arquivo q está na mesma pasta do programa
-dataset_Rocha.replace(['R', 'M'], [1, -1], inplace=True)
-X_Rocha = dataset_Rocha.iloc[:, 0:60].values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
-D_Rocha = dataset_Rocha.iloc[:, 60:].values
+dataset_Rochas = pd.read_csv('databases/sonar.all-data') # caminho relativo do arquivo q está na mesma pasta do programa
+dataset_Rochas.replace(['R', 'M'], [1, -1], inplace=True)
+X = dataset_Rochas.values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
+D = dataset_Rochas.values
+x = np.arange(len(dataset_Rochas))
+np.random.shuffle(x)
 
+X_Rochas_new = X[x]
+D_Rochas_new = D[x]
+
+lenTrain_Rochas = int(0.8*len(dataset_Rochas))
+
+X_Rochas = X_Rochas_new[0:lenTrain_Rochas,0:60]
+D_Rochas = D_Rochas_new[0:lenTrain_Rochas,60:]
+
+X_Rocha_teste = X_Rochas_new[lenTrain_Rochas:,0:60]
+D_Rocha_teste = D_Rochas_new[lenTrain_Rochas:,60:]
 
 #Preparando dados Iris
 dataset_Iris = pd.read_csv('databases/iris.data') # caminho relativo do arquivo q está na mesma pasta do programa
 dataset_Iris.replace(['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'], [1, -1, -1], inplace=True)
 X = dataset_Iris.values
-d = dataset_Iris.values
+D = dataset_Iris.values
 x = np.arange(len(dataset_Iris))
 np.random.shuffle(x)
 
 X_new = X[x]
-d_new = d[x]
+D_new = D[x]
 
-X_Iris = X_new.iloc[0:101, 0:4].values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
-#np.random.shuffle(X_Iris)
-D_Iris = d_new.iloc[0:101, 4:].values
-#np.random.shuffle(D_Iris)
+lenTrain = int(0.8*len(dataset_Iris))
 
-X_IrisTest = X_new.iloc[101:151, 0:4].values # o iloc gera uma subtabela ":" todas as linhas, da coluna 0 a 3, o ultimo numero não está incluso
-#np.random.shuffle(X_IrisTest)
-D_IrisTest = d_new.iloc[101:151:, 4:].values
-#np.random.shuffle(D_IrisTest)
+X_Iris = X_new[0:lenTrain,0:4]
+D_Iris = D_new[0:lenTrain,4:]
+
+X_IrisTest = X_new[lenTrain:,0:4]
+D_IrisTest = D_new[lenTrain:,4:]
+
 #Adaline
     #Adaline Iris
 print('Rodando Adaline para dados - Iris')
@@ -44,21 +55,21 @@ print('')
     #Adaline Rochas
 print('Rodando Adaline para dados - Rochas')
 
-a_Rocha = Adaline(len(X_Rocha[0]), epochs=1000)#epochs, quantidade de iteraçoes
-a_Rocha.train(X_Rocha, D_Rocha)
-a_Rocha.testNetwork(X_Rocha,D_Rocha)
+a_Rocha = Adaline(len(X_Rochas[0]), epochs=1000, labelGraphic = 'Adaline - Rochas')#epochs, quantidade de iteraçoes
+a_Rocha.train(X_Rochas, D_Rochas)
+a_Rocha.testNetwork(X_Rocha_teste,D_Rocha_teste)
 a_Rocha.Graphic()
 a_Rocha.showGraphic()
 print('')
 
-Perceptron
-    #Perceptron Iris
-print('Rodando Perceptron para dados - Iris')
-p_Iris = Perceptron(len(X_Iris[0]), epochs=100, labelGraphic = 'Perceptron - Iris') #epochs, quantidade de iteraçoes
-p_Iris.train(X_Iris, D_Iris)
-p_Iris.testNetwork(X_IrisTest,D_IrisTest)
-p_Iris.showConfusionMatrix()
-print('')
+#Perceptron
+#    #Perceptron Iris
+#print('Rodando Perceptron para dados - Iris')
+#p_Iris = Perceptron(len(X_Iris[0]), epochs=100, labelGraphic = 'Perceptron - Iris') #epochs, quantidade de iteraçoes
+#p_Iris.train(X_Iris, D_Iris)
+#p_Iris.testNetwork(X_IrisTest,D_IrisTest)
+#p_Iris.showConfusionMatrix()
+#print('')
     #Perceptron Rochas
 #print('Rodando Perceptron para dados - Rochas')
 
